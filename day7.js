101aec5ffe32392428f158af9ac5176e07c02541
Argomaru <48087677+Argomaru@users.noreply.github.com>
2022-06-05 15:42:59 +0700

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,107 @@
+//HTML
+//Bukan programming language
+//ga bisa proses data kek javascript
+//markup language
+//fungsinya untuk membuat keraangka dari suatu website
+
+/*
+HTML Tag ada 2 :
+Paired Tags dan Singular Tags
+paried Tag ex : <div> </div>
+                : <p> </p>
+Singular ex : <br>
+                : <img>
+<!DOCTYPE HTML> (buat ngasih tau versi html biar pas brower jalanin html, maka browser bisa tauu kita jalanin html versi ke berapa)
+Tag dari HTML
+<html lang="en">
+</html> (bahasa inggris) kalo kita pake bahasa lain dari bahasa yang di set ga bakalan error juga
+*/
+
+/*
+<head>
+(tempat buat tag2 lain yang fungsinya buat isi informasi tambahan)
+</head>
+*/
+
+/*
+<head>
+    <meta charset="UTF-8"> (kita ngasih tau sih browser bahwa kita bakal make berbagai macam karakter di html ini)
+</head>
+*/
+
+/*
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> jalanin berdasarkan reoslusi dari device
+*/
+
+/*
+
+<head>
+    <title>Document</title? (memunculkan judul adri halaman yang kita buka)
+</head>
+*/
+
+/*
+<body>
+    menampung konten yang akan ditunjukan di website
+</body>
+*/
+
+/*
+
+<a>
+buat hyperlink
+</a>
+*/
+
+/* common tag
+- Heading tag
+<h1></h1> ukuran paling besar
+sampe ke
+<h6></h6>ukuran paling kecil
+
+- paragraph tag
+<p> </p>
+buat bikin paragraph
+
+
+- Bold
+<b> </b>
+
+-underline
+<u> </u>
+
+-italic
+<i> </i>
+
+-<ol> </ol>
+buat digunakan sebagai menamah penomoran pada list
+
+-<ul> </ul>
+buletin list
+
+/*
+buat simple form regristrasi
+isinya ada : 
+nama 
+email 
+password 
+umur 
+alamat 
+tanggal lahir
+
+button submit => alert isi dari semua yang diisi di text tersebut -->
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
